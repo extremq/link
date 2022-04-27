e6aaae2e24f5e7b74a2dd241e9927bd696dfa51a
extremq <stefansteff999@gmail.com>
2022-04-27 22:13:32 +0300

    Set up auto-update.

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from arguments import *
 from socket_helpers import *
 
-import random, string
+import random, string, os
 
 def generate_password():
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(10))
 
 def main():
+    os.system("git pull")
     print("Welcome.\nSyntax: python server.py PORT")
     PORT = get_port()
     main_socket = setup_server(PORT)
